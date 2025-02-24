a37e8610227e9582c2f195352a516a07512ee6a3
VladislavDevs <supermars5327@gmail.com>
2025-02-24 19:30:54 +0300

    Доработал абстрактный класс Pet

--- Pets.py
+++ Pets.py
@@ -8,40 +8,41 @@ class Pet(abc.ABC):
     _health = 0
     _hunger = 0.0
 
-    def __init__(self, _eat, _drink, _voice, _thirst, _health, _hunger):
+    def __init__(self, _eat, _drink, _thirst, _health, _hunger):
         self._eat = _eat
         self._drink = _drink
-        self._voice = _voice
         self._thirst = _thirst
         self._health = _health
         self._hunger = _hunger
 
     @abc.abstractmethod
-    def eat(self): pass
+    def eat(self):
+        print(self._eat)
+        self._hunger = 0
+        return
 
     @abc.abstractmethod
-    def drink(self): pass
+    def drink(self):
+        print(self._drink)
+        self._thirst = 0
+        return
 
     @abc.abstractmethod
-    def voice(self): pass
+    def voice(self):
+        print(self._voice)
+        return
 
 class Cat(Pet):
-    def __init__(self, _eat, _drink, _voice, _thirst, _health, _hunger):
-        super().__init__(_eat, _drink, _voice, _thirst, _health, _hunger)
-
-        def eat(self): return "Хрум-Хрум"
-        def drink(self): return "Хлюп-Хлюп"
-        def voice(self): return "Мяу-Мяу"
+    def __init__(self, _eat, _drink, _thirst, _health, _hunger):
+        super().__init__(_eat, _drink, _thirst, _health, _hunger)
+        self._voice = "Мяу-Мяу"
 
         def walk(self): return "Топ-Топ"
 
 class Dog(Pet):
-    def __init__(self, _eat, _drink, _voice, _thirst, _health, _hunger):
-        super().__init__(_eat, _drink, _voice, _thirst, _health, _hunger)
-
-        def eat(self): return "Хрум-Хрум"
-        def drink(self): return "Хлюп-Хлюп"
-        def voice(self): return "Гав-Гав"
+    def __init__(self, _eat, _drink, _thirst, _health, _hunger):
+        super().__init__(_eat, _drink, _thirst, _health, _hunger)
+        self._voice = "Гав-Гав"
 
         def walk(self): return "Топ-Топ"
 
