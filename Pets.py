8bdfa982a73f50f1f52e1732917de9a08a6a50ac
murik0586 <murikmusaev86@gmail.com>
2025-03-23 16:44:14 +0300

    add comments

--- Pets.py
+++ Pets.py
@@ -1,6 +1,7 @@
 import abc
 
 class Pet(abc.ABC):
+
     # Инициализация полей (в некоторых яп называют переменными)
     def __init__(self, eat : str, drink : str, thirst : float, health : int, hunger : float, voice : str): #Конструктор
         self.__eat = eat # Звук еды (Заглушка для тестирования, переделаем на анимации)
@@ -10,41 +11,44 @@ class Pet(abc.ABC):
         self.__hunger = hunger # Голод
         self.__voice = voice # Голос животного
 
-    @abc.abstractmethod
-    def get_eat(self):
-        print(self.__eat)
-        self.__hunger = 0
-        return
 
-    @abc.abstractmethod
+    def get_eat(self):
+        """
+        Порвывавы
+        """
+
+        return self.__eat
+
+
     def set_eat(self, eat : str):
         self.__eat = eat
         return
 
-    @abc.abstractmethod
+
     def get_drink(self):
         print(self.__drink)
         self.__thirst = 0
         return
 
-    @abc.abstractmethod
+
     def set_drink(self, drink : str):
         self.__drink = drink
         return
 
-    @abc.abstractmethod
+
     def get_voice(self):
+        """difdsfasf"""
         print(self.__voice)
         return
 
-    @abc.abstractmethod
+
     def set_voice(self, voice : str):
         self.__voice = voice
         return
 
 class Cat(Pet):
-    def __init__(self, eat, drink, thirst, health, hunger):
-        super().__init__(eat, drink, thirst, health, hunger)
+    def __init__(self, eat, drink, thirst, health, hunger,voice):
+        super().__init__(eat, drink, thirst, health, hunger,voice)
         self.__voice = "Мяу-Мяу"
 
     def get_walk(self): return "Топ-Топ"
@@ -53,7 +57,7 @@ class Cat(Pet):
 
 class Dog(Pet):
     def __init__(self, eat, drink, thirst, health, hunger):
-        super().__init__(eat, drink, thirst, health, hunger)
+        super().__init__(eat, drink, thirst, health, hunger,"sfasfs")
         self.__voice = "Гав-Гав"
 
     def get_walk(self): return "Топ-Топ"
